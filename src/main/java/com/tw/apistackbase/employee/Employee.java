c6986ec1e0f73d4fcc0d9da92dbd7f5cc72c2f99
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-15 20:04:15 +0800

    created Employee

---
+++ src/main/java/com/tw/apistackbase/employee/Employee.java
@@ -0,0 +1,40 @@
+package com.tw.apistackbase.employee;
+
+public class Employee {
+    private String employeeID;
+    private String name ;
+    private int age;
+    private String gender;
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
